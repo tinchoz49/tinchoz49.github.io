452fec904833aab0af748e06149d31cb625805a4
Martin Acosta <tinchoz49@gmail.com>
2024-07-16 11:06:11 -0300

    generate og:image dynamically

--- src/components/OG.tsx
+++ src/components/OG.tsx
@@ -9,9 +9,9 @@ export default function OG(options: Options) {
 
   return (
     <div tw="flex size-full flex-col items-center justify-center bg-white">
-      <div tw="flex w-full bg-gray-50">
+      <div tw="flex w-full bg-gray-50" style={{ width: 1200, height: 630 }}>
         <div tw="flex w-full justify-center p-8 px-4 py-12 md:items-center">
-          <h2 tw="flex-2 flex flex-col text-left text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          <h2 tw="flex-2 flex flex-col text-left text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl leading-normal">
             <span>{title}</span>
             <span tw="text-indigo-600">{description}</span>
           </h2>
